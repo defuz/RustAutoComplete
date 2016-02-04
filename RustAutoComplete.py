138a2ba56546d94bd3bc2dce142a914c2ccba75a
defuz <defuz.net@gmail.com>
2016-02-04 22:29:13 +0200

    using sublime.platform()

--- RustAutoComplete.py
+++ RustAutoComplete.py
@@ -1,7 +1,6 @@
 import os
 import sublime
 import sublime_plugin
-import platform
 import re
 import subprocess
 import tempfile
@@ -210,7 +209,7 @@ class RustGotoDefinitionCommand(sublime_plugin.TextCommand):
             path = result.path
             # On Windows the racer will return the paths without the drive
             # letter and we need the letter for the open_file to work.
-            if platform.system() == 'Windows' and not re.compile('^\w\:').match(path):
+            if sublime.platform() == 'windows' and not re.compile('^\w\:').match(path):
                 path = 'c:' + path
             encoded_path = "{0}:{1}:{2}".format(path, result.row, result.column)
             self.view.window().open_file(encoded_path, sublime.ENCODED_POSITION)
