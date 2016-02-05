5135cf5d0cd8d5e4cefbb393acb50dba9734e00a
defuz <defuz.net@gmail.com>
2016-02-05 04:28:39 +0200

    Using daemon mode and stdin instead of tmp files

--- RustAutoComplete.py
+++ RustAutoComplete.py
@@ -7,159 +7,100 @@ import tempfile
 from subprocess import Popen, PIPE
 
 
-settings = None
-
-
-class Settings:
-    def __init__(self):
+class Racer:
+    def load(self):
         package_settings = sublime.load_settings("RustAutoComplete.sublime-settings")
-        package_settings.add_on_change("racer", settings_changed)
-        package_settings.add_on_change("search_paths", settings_changed)
+        package_settings.add_on_change("racer", self.reload)
+        package_settings.add_on_change("search_paths", self.reload)
 
         self.racer_bin = package_settings.get("racer", "racer")
-        self.search_paths = package_settings.get("search_paths", [])
+        search_paths = package_settings.get("search_paths", [])
         self.package_settings = package_settings
 
+        # Copy the system environment and add the source search
+        # paths for racer to it
+        env = os.environ.copy()
+        expanded_search_paths = [os.path.expanduser(path) for path in search_paths]
+        if 'RUST_SRC_PATH' in env:
+            expanded_search_paths.append(env['RUST_SRC_PATH'])
+        env['RUST_SRC_PATH'] = os.pathsep.join(expanded_search_paths)
+
+        # Run racer
+        startupinfo = None
+        if os.name == 'nt':
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        self.process = Popen([self.racer_bin, "-i", "tab-text", "daemon"],
+                             stdin=PIPE, stdout=PIPE, stderr=PIPE,
+                             universal_newlines=True, bufsize=0,
+                             env=env, startupinfo=startupinfo)
+
     def unload(self):
-        self.package_settings.clear_on_change("racer")
-        self.package_settings.clear_on_change("search_paths")
+        if hasattr(self, 'package_settings'):
+            self.package_settings.clear_on_change("racer")
+            self.package_settings.clear_on_change("search_paths")
+        if hasattr(self, 'process'):
+            self.process.kill()
+
+    def reload(self):
+        self.unload()
+        self.load()
+
+    def run_command(self, args, content):
+        self.process.stdin.write('\t'.join(args))
+        self.process.stdin.write('\n')
+        self.process.stdin.write(content)
+        self.process.stdin.write('\x04')
+
+        print("input ", '\t'.join(args), '\n', content)
+
+        returncode = self.process.poll()
+        if returncode is not None:
+            print("%s is failed with exit code %s and stderr:" % (self.racer_bin, returncode))
+            print(self.process.stderr.read())
+            sublime.error_message("Racer quit unexpectedly. See console for more info.")
+            self.process = None
+            self.reload()
+            return []
+
+        results = []
+
+        while True:
+            line = self.process.stdout.readline()
+            parts = line.rstrip().split('\t')
+            if parts[0] == 'MATCH':
+                if len(parts) == 7: # without snippet
+                    parts.insert(2, None)
+                results.append(Result(parts))
+                continue
+            if parts[0] == 'END':
+                break
+
+        return results
+
+    def complete_with_snippet(self, row, col, filename, content):
+        args = ["complete-with-snippet", str(row), str(col), filename, '-']
+        return self.run_command(args, content)
+
+    def find_definition(self, row, col, filename, content):
+        args = ["find-definition", str(row), str(col), filename, '-']
+        return self.run_command(args, content)
 
 
-def plugin_loaded():
-    global settings
-    settings = Settings()
-
-
-def plugin_unloaded():
-    global settings
-    if settings != None:
-        settings.unload()
-        settings = None
-
-
-def settings_changed():
-    global settings
-    if settings != None:
-        settings.unload()
-        settings = None
-    settings = Settings()
+racer = Racer()
+plugin_loaded = racer.load
+plugin_unloaded = racer.unload
 
 
 class Result:
     def __init__(self, parts):
-        self.completion = parts[0]
-        self.snippet = parts[1]
-        self.row = int(parts[2])
-        self.column = int(parts[3])
-        self.path = parts[4]
-        self.type = parts[5]
-        self.context = parts[6]
-
-
-def expand_all(paths):
-    return [os.path.expanduser(path)
-            for path in paths]
-
-
-def determine_save_dir(view):
-    # If we return None then it will fall back on the system tmp directory
-    save_dir = None
-
-    # Try to save to the same directory the file is saved in
-    if view.file_name() is not None:
-        save_dir = os.path.dirname(view.file_name())
-
-    # If the file has not been saved, and the window has a folder open,
-    # try to treat the main folder as if it were a cargo project
-    source_folder = ""
-    if len(view.window().folders()) > 0:
-        source_folder = os.path.join(view.window().folders()[0], "src")
-    if save_dir is None and os.path.isdir(source_folder):
-        save_dir = source_folder
-
-    # If nothing else has worked, look at the folders that other open files are in
-    if save_dir is None:
-        paths = [v.file_name() for v in view.window().views() if v.file_name() is not None]
-        # We only care about open rust files
-        paths = [path for path in paths if path[-3:] == ".rs"]
-        directories = [os.path.dirname(path) for path in paths]
-        if len(directories) == 0:
-            return None
-
-        # Count the frequency of occurance of each path
-        dirs = {}
-        for item in directories:
-            if item not in dirs:
-                dirs[item] = 1
-            else:
-                dirs[item] += 1
-
-        # Use the most common path
-        save_dir = max(dirs.keys(), key=(lambda key: dirs[key]))
-
-    return save_dir
-
-
-def run_racer(view, cmd_list):
-    # Retrieve the entire buffer
-    region = sublime.Region(0, view.size())
-    content = view.substr(region)
-    with_snippet = cmd_list[0] == "complete-with-snippet"
-
-    # Figure out where to save the temp file so that racer can do
-    # autocomplete based on other user files
-    save_dir = determine_save_dir(view)
-
-    # Save that buffer to a temporary file for racer to use
-    temp_file = tempfile.NamedTemporaryFile(mode='w', encoding='utf-8', delete=False, dir=save_dir)
-    temp_file_path = temp_file.name
-    temp_file.write(content)
-    temp_file.close()
-    cmd_list.insert(0, settings.racer_bin)
-    cmd_list.append(temp_file_path)
-
-    # Copy the system environment and add the source search
-    # paths for racer to it
-    env = os.environ.copy()
-    expanded_search_paths = expand_all(settings.search_paths)
-    if 'RUST_SRC_PATH' in env:
-        expanded_search_paths.append(env['RUST_SRC_PATH'])
-    env['RUST_SRC_PATH'] = os.pathsep.join(expanded_search_paths)
-
-    # Run racer
-    startupinfo = None
-    if os.name == 'nt':
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
-    process = Popen(cmd_list, stdout=PIPE, stderr=PIPE, env=env, startupinfo=startupinfo)
-    (output, err) = process.communicate()
-    exit_code = process.wait()
-
-    # Remove temp file
-    os.remove(temp_file_path)
-
-    # Parse results
-    results = []
-    match_string = "MATCH "
-    if exit_code == 0:
-        for byte_line in output.splitlines():
-            line = byte_line.decode("utf-8")
-            if line.startswith(match_string):
-                if with_snippet:
-                    parts = line[len(match_string):].split(';', 7)
-                else:
-                    parts = line[len(match_string):].split(',', 6)
-                    parts.insert(1, "")
-
-                result = Result(parts)
-                if result.path == view.file_name():
-                    continue
-                if result.path == temp_file_path:
-                    result.path = view.file_name()
-                results.append(result)
-    else:
-        print("CMD: '%s' failed: exit_code:" % ' '.join(cmd_list), exit_code, output, err)
-    return results
+        self.completion = parts[1]
+        self.snippet = parts[2]
+        self.row = int(parts[3])
+        self.column = int(parts[4])
+        self.path = parts[5]
+        self.type = parts[6]
+        self.context = parts[7]
 
 
 class RustAutocomplete(sublime_plugin.EventListener):
@@ -167,43 +108,37 @@ class RustAutocomplete(sublime_plugin.EventListener):
         # Check if this is a Rust source file. This check
         # relies on the Rust syntax formatting extension
         # being installed - https://github.com/jhasse/sublime-rust
-        if view.match_selector(locations[0], "source.rust"):
-            # Get the buffer location in correct format for racer
-            row, col = view.rowcol(locations[0])
-            row += 1
+        if not view.match_selector(locations[0], "source.rust"):
+            return None
 
-            try:
-                raw_results = run_racer(view, ["complete-with-snippet", str(row), str(col)])
-            except FileNotFoundError:
-                print("Unable to find racer executable (check settings)")
-                return
+        row, col = view.rowcol(locations[0])
+        region = sublime.Region(0, view.size())
+        content = view.substr(region)
+        raw_results = racer.complete_with_snippet(row+1, col, view.file_name(), content)
 
-            results = []
-            lalign = 0;
-            ralign = 0;
-            for result in raw_results:
-                result.middle = "{0} ({1})".format(result.type, os.path.basename(result.path))
-                lalign = max(lalign,len(result.completion)+len(result.middle))
-                ralign = max(ralign, len(result.context))
+        results = []
+        lalign = 0
+        ralign = 0
+        for result in raw_results:
+            result.middle = "{0} ({1})".format(result.type, os.path.basename(result.path))
+            lalign = max(lalign, len(result.completion)+len(result.middle))
+            ralign = max(ralign, len(result.context))
 
-            for result in raw_results:
-                context = result.context
-                result = "{0} {1:>{3}} : {2:{4}}".format(result.completion, result.middle, result.context, lalign - len(result.completion), ralign), result.snippet
-                results.append(result)
-            if len(results) > 0:
-                # return list(set(results))
-                return (list(set(results)),
-                        sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
+        for result in raw_results:
+            context = result.context
+            result = "{0} {1:>{3}} : {2:{4}}".format(result.completion, result.middle, result.context, lalign - len(result.completion), ralign), result.snippet
+            results.append(result)
+        if results:
+            return (list(results),
+                    sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS)
 
 
 class RustGotoDefinitionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        # Get the buffer location in correct format for racer
         row, col = self.view.rowcol(self.view.sel()[0].begin())
-        row += 1
-
-        results = run_racer(self.view, ["find-definition", str(row), str(col)])
-
+        region = sublime.Region(0, self.view.size())
+        content = self.view.substr(region)
+        results = racer.find_definition(row+1, col, self.view.file_name(), content)
         if len(results) == 1:
             result = results[0]
             path = result.path
@@ -211,5 +146,5 @@ class RustGotoDefinitionCommand(sublime_plugin.TextCommand):
             # letter and we need the letter for the open_file to work.
             if sublime.platform() == 'windows' and not re.compile('^\w\:').match(path):
                 path = 'c:' + path
-            encoded_path = "{0}:{1}:{2}".format(path, result.row, result.column)
+            encoded_path = "{0}:{1}:{2}".format(path, result.row, result.column+1)
             self.view.window().open_file(encoded_path, sublime.ENCODED_POSITION)
